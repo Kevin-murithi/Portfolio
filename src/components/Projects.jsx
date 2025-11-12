534af2b625ef187a1fb67a430a8ba9ffe81adc16
Kevin-murithi <121344012+Kevin-murithi@users.noreply.github.com>
2025-11-12 07:55:30 +0300

    Projects update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -141,7 +141,6 @@ const Projects = () => {
           A showcase of innovative solutions and creative implementations,
           each project representing a unique challenge solved with modern technologies
         </p>
-        <p> Project Live demos and Source code restricted for copyright reasons. Contact me to view my work</p>
       </motion.div>
 
       {/* Projects Grid */}
